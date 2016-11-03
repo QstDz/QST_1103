1b4547729a44524415fad78ac4719e86942ec269
DongZhuang <15589823183@163.com>
2016-11-03 14:17:50 +0800

    dongzhuang_ex2

--- ex2/DifferentFormat.java
+++ ex2/DifferentFormat.java
@@ -1,3 +1,5 @@
+
+
 package No2;
 
 import java.sql.Timestamp;
@@ -13,13 +15,14 @@ import java.util.Scanner;
  * 1. 修改代码，使程序在输入『31/Dec/2015:00:02:09』的时候，转化为时间戳并输出，把结果填写到README.md当中
  * 2. 和ex1对比，对多出来的代码进行注释
  * 3. 提交代码到分支下，创建pull request，与主仓库的master分支对比
+ * 
+ * 1451491320009
  */
 public class DifferentFormat {
 	@SuppressWarnings("resource")
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
-		Locale locale = Locale.US; 
-		SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", locale); //修改格式
+		SimpleDateFormat inputFormat = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:SS", Locale.ENGLISH); //修改格式,因为月份是三个英文字母的，而且年和小时之间是冒号，所以需要这种格式，还需要locale.english对英文月份进行识别
 		while (scanner.hasNext()){
 			String line = scanner.nextLine();
 			Date lineDate = null;
