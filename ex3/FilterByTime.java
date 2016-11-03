dd6ef7341cdb095690289fe1c01e40fa92f868ed
DongZhuang <15589823183@163.com>
2016-11-03 14:40:49 +0800

    dongzhuang_ex3

--- ex3/FilterByTime.java
+++ ex3/FilterByTime.java
@@ -2,10 +2,14 @@ package No3;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /*
  * 题目要求：
@@ -21,17 +25,35 @@ public class FilterByTime {
 		SimpleDateFormat regularFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		Date beginDate = regularFormat.parse("2015-12-31 18:00:00");
 		Date endDate = regularFormat.parse("2015-12-31 19:00:00");
-		String filePath = "./access.log";
+		//String filePath = "./access.log";
+		String filePath = "e://data.txt";
 		FileInputStream inputStream = new FileInputStream(filePath);
+		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(inputStream, "UTF-8");
 		while (scanner.hasNext()){
 			// 对每行进行处理
 			String line = scanner.nextLine();
-			// 切分获取IP，Time
-			String strIp = null;
-			String strTime = null;
-			// 对在时间区间内的数据进行输出
-			System.out.println(strIp + "\t" + strTime);
+			Pattern pattern=Pattern.compile("(\\d+.\\d+.\\d+.\\d+).*(\\[.*\\])");//正则提取ip和时间
+			Matcher matcher=pattern.matcher(line);
+			if(matcher.find()){				
+				String strIp = null;
+				String strTime = null;
+				
+				strIp=matcher.group(1);
+				
+				String t1[]=matcher.group(2).split(" ");//对提取到的时间进一步优化
+				strTime=t1[0].substring(1, t1[0].length());
+				Date date=new Date();
+				DateFormat df=new SimpleDateFormat("dd/MMM/yyyy:HH:mm:SS",Locale.ENGLISH);
+				date=df.parse(strTime);//将字符串解析成时间
+				
+
+				if(date.after(beginDate) && date.before(endDate)){//时间判断
+					System.out.println(strIp+"\t"+date);
+				}
+
+			}
+
 		}
 	}
 	
