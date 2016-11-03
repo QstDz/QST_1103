5924f6c40e36f1ac203a11ab33feba3d5275337f
DongZhuang <15589823183@163.com>
2016-11-03 13:50:10 +0800

    Update TimestampTransfer.java

--- ex1/TimestampTransfer.java
+++ ex1/TimestampTransfer.java
@@ -1,5 +1,4 @@
-package No1;
-
+package qst.dz.ex;
 import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -12,21 +11,22 @@ import java.util.Scanner;
  * 1. 在个人分支下，修改Answers.md文件，里面填入当输入为'2016-11-11 11:11:11'时，输出的值是多少
  * 2. 对代码进行注释，说明每行代码的作用，把代码提交到个人分支下
  * 3. 创建pull request，与主仓库的master分支对比
+ * 2016/11/11 11:11:11 to 1478833871000
  */
-public class TimestampTransfer {
+public class ex1 {
 	@SuppressWarnings("resource")
 	public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);
-		SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-		while (scanner.hasNext()){
-			String line = scanner.nextLine();
-			Date lineDate = null;
-			long lineTimestamp;
+		Scanner scanner = new Scanner(System.in);//获取输入
+		SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//定义输入的时间格式
+		SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");//定义输出的时间格式
+		while (scanner.hasNext()){//检测输入
+			String line = scanner.nextLine();//定义一个字符串获取输入
+			Date lineDate = null;//定义一个Date类型的变量
+			long lineTimestamp;//定义一个long类型的变量timestamp
 			try {
-				lineDate = inputFormat.parse(line);
-				lineTimestamp = lineDate.getTime();
-				System.out.println(outputFormat.format(lineDate) + " to " + lineTimestamp);
+				lineDate = inputFormat.parse(line);//对输入的时间进行格式转换
+				lineTimestamp = lineDate.getTime();//将lineDate转换成long类型并赋给linetimestamp
+				System.out.println(outputFormat.format(lineDate) + " to " + lineTimestamp);//输出结果
 			} catch (ParseException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
